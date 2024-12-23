0f3d0462d9e978f3d6b66c40e16edadcedd21f54
Ravi Teja <ladi.ravi884781@gmail.com>
2024-12-23 13:40:27 +0530

    Remove tests for useVaultAccess hook and export StateVaultContext

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { useVaultAccess } from './StateVault/useVaultAccess'
 import { configureVault } from './StateVault/configureVault'
 import { createSafeDepositBox } from './StateVault/createSafeDepositBox'
 import { createKey } from './StateVault/createKey'
+import { StateVaultContext } from './StateVault/StateVaultContext'
 
 export {
   VaultGuard,
@@ -12,4 +13,5 @@ export {
   createKey,
   useVaultKey,
   useVaultAccess,
+  StateVaultContext,
 }
